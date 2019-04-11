4c8a32fadba4be33b984726622dc715cd743aec2
robiwankenobi <rfnkurle@gmail.com>
2019-04-10 21:44:25 -0700

    Burger 1.0

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
+INSERT INTO burgers (burger_name) VALUES ('People Burger');
