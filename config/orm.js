2b523a21f74e04c4ecdff7a25cac696358a79ebb
robiwankenobi <rfnkurle@gmail.com>
2019-04-10 22:28:48 -0700

    wtf burger

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,6 @@
 var connection = require("../config/connection.js");
 
+
 // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
 // In order to write the query, we need 3 question marks.
--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,7 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var cat = {
+var burger = {
   all: function(cb) {
     orm.all("burgers", function(res) {
       cb(res);
@@ -18,11 +18,7 @@ var cat = {
       cb(res);
     });
   },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
+  
 };
 
 // Export the database functions for the controller (catsController.js).
