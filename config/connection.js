334f3f5291abce3325109a2cba165866e4de7ee1
robiwankenobi <rfnkurle@gmail.com>
2019-04-12 19:12:44 -0700

    Ugh

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,7 @@ var connection;
   } else {
     connection = mysql.createConnection({
       host: "localhost",
+      port: 8889,
       user: "root",
       password: "root",
       database: "burgers_db"
